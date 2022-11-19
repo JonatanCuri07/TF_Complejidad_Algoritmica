98218a556e2c1ef1230b8d3b1cac182d33109b7c
JonatanCuri07 <u201912404@upc.edu.pe>
2022-11-19 16:16:10 -0500

    Added function to order by priority

--- main.py
+++ main.py
@@ -19,4 +19,23 @@ def making_adjacency_list(hospitals_csv, farmacias_csv, max_range, price = "Econ
       dis = distance_between_points(hospitals_csv[x],farmacias_csv[y]) #obtener la distancia entre el hospital y la farmacia
       if dis <= max_range and farmacias_csv[y][4] == price: #se compara si la farmacia cumple requisitos del usuario
         hospitals[x].append((farmacias_csv[y], dis))
-  return hospitals
+  return hospitals #se retorna arreglo de hospitales con sus farmacias cercanas
+
+def divideyvenceras(hospital):
+    n = len(hospital)
+    if n == 0:
+        return[]
+    current = hospital[0]
+    lst_left = []
+    lst_right = []
+    for i in range(n-1):
+      next = hospital[i+1]
+      lst_right.append(next) if next[1] > current[1] else lst_left.append(next)
+
+    if not len(lst_left)<=1:
+        lst_left=divideyvenceras(lst_left)
+    if not len(lst_right)<=1:
+        lst_right=divideyvenceras(lst_right)
+    current = [current]
+    hospital = lst_left + current + lst_right
+    return(hospital)
